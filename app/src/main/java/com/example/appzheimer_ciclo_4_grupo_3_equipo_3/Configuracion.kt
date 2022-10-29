54fb7e47d5f94347963ef6834a39991901c43d3a
georgedancal <gedanc@gmail.com>
2022-10-28 20:21:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appzheimer_ciclo_4_grupo_3_equipo_3/Configuracion.kt
@@ -0,0 +1,11 @@
+package com.example.appzheimer_ciclo_4_grupo_3_equipo_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Configuracion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuracion)
+    }
+}
---
+++ app/src/main/java/com/example/appzheimer_ciclo_4_grupo_3_equipo_3/Crear_Notas.kt
@@ -0,0 +1,11 @@
+package com.example.appzheimer_ciclo_4_grupo_3_equipo_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Crear_Notas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_notas)
+    }
+}
