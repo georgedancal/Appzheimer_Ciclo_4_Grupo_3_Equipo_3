7fe7ad2b58ff0f80a1c312c8feb75d5a4d6f43d3
georgedancal <gedanc@gmail.com>
2022-11-12 02:10:39 -0500

    Vista del splash

--- app/src/main/java/com/example/appzheimer_ciclo_4_grupo_3_equipo_3/view/ui/activities/Login.kt
+++ app/src/main/java/com/example/appzheimer_ciclo_4_grupo_3_equipo_3/view/ui/activities/Login.kt
@@ -1,7 +1,8 @@
-package com.example.appzheimer_ciclo_4_grupo_3_equipo_3
+package com.example.appzheimer_ciclo_4_grupo_3_equipo_3.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appzheimer_ciclo_4_grupo_3_equipo_3.R
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/appzheimer_ciclo_4_grupo_3_equipo_3/view/ui/activities/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.appzheimer_ciclo_4_grupo_3_equipo_3.view.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.appzheimer_ciclo_4_grupo_3_equipo_3.R
+import com.example.appzheimer_ciclo_4_grupo_3_equipo_3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.animationView.setAnimation(R.raw.animacionlottie)
+        binding.animationView.playAnimation()
+
+        handler= Handler(Looper.myLooper()!!)
+        handler.postDelayed(
+            {
+                val intent= Intent(this, Login::class.java)
+                startActivity(intent)
+                finish()
+            }, 4000
+        )
+
+    }
+}
